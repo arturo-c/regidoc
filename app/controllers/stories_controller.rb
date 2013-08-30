4c2b5ee0730e87bbd97ea46601feb702f1dfed4c
Arturo Contreras <arturo@allplayers.com>
2013-08-30 12:28:55 -0500

    Adding dynamic project selection.

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,10 +1,15 @@
 class StoriesController < ApplicationController
   def index
-    @projects = get_projects
-    return redirect_to :controller => :users, :action => :logout if @projects.nil?
+    projects = get_projects
+    return redirect_to :controller => :users, :action => :logout if projects.nil?
     @stories = Story.all
     @story = Story.new
-    puts @projects.to_yaml
+    prs = []
+    projects.each do |project|
+      prs << project.name
+      prs << project.id
+    end
+    @projects = Hash[*prs.flatten]
     respond_to do |format|
       format.html
       format.js {@stories}
@@ -38,7 +43,7 @@ class StoriesController < ApplicationController
   def create_pivotal_story
     PivotalTracker::Client.token = session[:pivotal_token]
     @story = Story.find(params[:id])
-    @project = PivotalTracker::Project.find(263731)
+    @project = PivotalTracker::Project.find(@story.project.to_i)
     @pivotal_story = @project.stories.create(:name => @story.title, :story_type => @story.story_type)
     @story.pivotal_id = @pivotal_story.id
     @story.save
@@ -47,12 +52,12 @@ class StoriesController < ApplicationController
   private
 
   def story_params
-    params.require(:story).permit(:title, :story_type, :status, :blocker, :date, :created_by, :assigned_to)
+    params.require(:story).permit(:title, :project, :story_type, :status, :blocker, :date, :created_by, :assigned_to)
   end
 
   def delete_story
     PivotalTracker::Client.token = session[:pivotal_token]
-    project = PivotalTracker::Project.find(263731)
+    project = PivotalTracker::Project.find(@story.project.to_i)
     project.stories.find(@story.pivotal_id).delete
   end
 
